fc650adf768abb43d5068d5d48250e70f3af1ea5
mika1407 <koneautomaatio@hotmail.com>
2026-03-02 23:54:16 +0200

    first commit background color added

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+FPS = 60
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 4, 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS 
+
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VEL = 20
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    pygame.display.update()
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                run = False
+                break
+             
+        draw(window)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main(WINDOW)
