c527dc72af77e929b0f85505717a8ede650a80f2
mika1407 <koneautomaatio@hotmail.com>
2026-03-03 00:23:15 +0200

    grid added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 pygame.init()
 
 FPS = 60
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 600, 600
 ROWS, COLS = 4, 4
 
 RECT_HEIGHT = HEIGHT // ROWS
@@ -22,8 +22,21 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
 def draw(window):
     window.fill(BACKGROUND_COLOR)
+    draw_grid(window)
+
     pygame.display.update()
 
 def main(window):
